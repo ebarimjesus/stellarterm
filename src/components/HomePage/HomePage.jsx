c820833222a8c0f00fc34357d5a56b67819b41f0
ebarimjesus <130609758+ebarimjesus@users.noreply.github.com>
2023-09-22 04:50:48 +0000

    update

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -139,7 +139,7 @@ export default class HomePage extends React.Component {
     return tokenData.map((token, index) => (
       <div className="token-card" key={index}>
         <a href={token.url} target="_blank" rel="noopener noreferrer">
-          <img src={token.imageSrc} alt={`${token.name} Token`} width="180" height="180" />
+          <img src={token.imageSrc} alt={`${token.name} Token`} width="185" height="185" />
         </a>
         <p>{`Buy ${token.name}`}</p>
         <p>{token.description}</p> {/* Render the description */}
